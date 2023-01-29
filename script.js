4685c9a9ccab229a7dcef8a691c95d4114b48289
Mohammed Fahim khan <42889314+ifahimkhan@users.noreply.github.com>
2023-01-29 13:03:12 +0530

    Add files via upload
    commit to wish happy birthday

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleImage() {
+    var image = document.getElementById("gift-image");
+    if (image.style.display === "none") {
+        image.style.display = "block";
+    } else {
+        image.style.display = "none";
+    }
+}
